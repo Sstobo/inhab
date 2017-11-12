9025b7be68515295f9d8c975ca95decbe1018f12
Sstobo <seanstobo@gmail.com>
2017-11-12 12:29:50 -0800

    header, footer, about

---
+++ themes/inhab/footer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div>
+			<!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="footer-container">	
+				<!-- footer block elements -->
+			<div class="footer-block">
+				<h2>Contect Info</h2>
+				<p>text</p>
+				<p>text</p>
+				<p>text</p>
+					 </div>
+			<div class="footer-block">
+			<h2>Contect Info</h2>
+				<p>text</p>
+				<p>text</p>
+				<p>text</p>
+			 </div>
+			<div class="footer-block"> 
+				<div class="footer-logo"></div>
+			</div>
+			</div>   
+				<div class="site-info">
+					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
