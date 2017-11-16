4eb48f337ba038cc911228c19a8efb79bedf8e65
Sstobo <seanstobo@gmail.com>
2017-11-16 11:45:52 -0800

    archive page active

--- themes/inhab/archive.php
+++ themes/inhab/archive.php
@@ -7,8 +7,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="products-content-area">
+		<main id="main" class="products-site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
@@ -19,25 +19,33 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php endif; ?>
 
+<!-- WRAPPER FOR ALL PRODUCTS -->
+			<div class="product-block-wrapper">
 				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
+		
+				global $post;
+				$args = array( 'posts_per_page' => 16,'post_type' => 'product', 'order' => 'ASC' ); 
+		
+				$posts = get_posts( $args );
+				foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+					
+						<div class="product-block-wrapper">
+						<div class="product-block">
+						<?php the_post_thumbnail( 'medium'); ?>
+						<div class="content-block">
+							<p><?php the_date(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></p>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></br>
+							<a href="<?php the_permalink(); ?>" class="button">Read More</a>
+						</div>
+					</div>
+</div>
+		<?php endforeach; 
+		wp_reset_postdata();?>
 		</main><!-- #main -->
+
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
